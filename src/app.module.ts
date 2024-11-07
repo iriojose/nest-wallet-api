457cf2995a7f1837577a4ca89df3b91c9c3480d5
iriojose <iriojgomezv@gmail.com>
2024-11-07 12:13:31 -0400

    ajustes para validacion de datos para el deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './lib/prisma.service';
 import { PaymentModule } from './payment/payment.module';
+import { ConfigModule } from '@nestjs/config';
+import { deploymentValidationSchema } from './validations/schemas/deployment';
 
 @Module({
-  imports: [UsersModule, PaymentModule],
-  controllers: [],
-  providers: [PrismaService],
+    imports: [
+		ConfigModule.forRoot({
+			isGlobal: true, 
+			validate: (config) => {
+				const result = deploymentValidationSchema.safeParse(config);
+				if (!result.success) {
+				  throw new Error('Variables de entorno no validas');
+				}
+				return config;
+			},
+		}),
+    	UsersModule, PaymentModule
+    ],
+    providers: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodError } from 'zod';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors()
+
+	try {
+		await app.listen(3000);
+	} catch (error) {
+		console.log(error)
+	}
 }
 bootstrap();
+
 
